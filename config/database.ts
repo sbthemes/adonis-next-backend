df4be2e669e614123b0060369245e7f2a728352d
Shailesh Savaliya <sbthemes@gmail.com>
2022-09-16 11:32:07 +0530

    database setup (lucid)

---
+++ config/database.ts
@@ -0,0 +1,27 @@
+import Env from '@ioc:Adonis/Core/Env'
+import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+
+const databaseConfig: DatabaseConfig = {
+    connection: Env.get('DB_CONNECTION'),
+
+    connections: {
+        mysql: {
+            client: 'mysql2',
+            connection: {
+                host: Env.get('MYSQL_HOST'),
+                port: Env.get('MYSQL_PORT'),
+                user: Env.get('MYSQL_USER'),
+                password: Env.get('MYSQL_PASSWORD', ''),
+                database: Env.get('MYSQL_DB_NAME'),
+                charset: 'utf8mb4',
+            },
+            migrations: {
+                naturalSort: true,
+            },
+            healthCheck: false,
+            debug: false,
+        },
+    },
+}
+
+export default databaseConfig
